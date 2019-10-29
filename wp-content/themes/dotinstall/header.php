00fa792ad0ee2668beb3b94c7535ae99cd822a25
Taishi Asato <defense433@gmail.com>
2019-10-29 20:28:51 +0900

    更新

--- wp-content/themes/dotinstall/header.php
+++ wp-content/themes/dotinstall/header.php
@@ -5,9 +5,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
         <title><?php wp_title('|', true, 'right'); bloginfo('name'); ?></title>
 	      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-        <link rel="stylesheet" media="screen and (min-width: 897px)" href="<?php echo get_template_directory_uri(); ?>/style.css" type="text/css" />
-        <link rel="stylesheet" media="screen and (min-width: 481px) and (max-width: 896px)" href="<?php echo get_template_directory_uri(); ?>/style_tb.css" type="text/css" />
-        <link rel="stylesheet" media="screen and (max-width: 480px)" href="<?php echo get_template_directory_uri(); ?>/style_sp.css" type="text/css" />
+        <link rel="stylesheet" media="screen and (min-width: 897px)" href="https://wordpressradiotest.herokuapp.com/wp-content/themes/dotinstall/style.css" type="text/css" />
+        <link rel="stylesheet" media="screen and (min-width: 481px) and (max-width: 896px)" href="https://wordpressradiotest.herokuapp.com/wp-content/themes/dotinstall/style_tb.css" type="text/css" />
+        <link rel="stylesheet" media="screen and (max-width: 480px)" href="http://wordpressradiotest.herokuapp.com/wp-content/themes/dotinstall/style_sp.css" type="text/css" />
+        http://wordpressradiotest.herokuapp.com/wp-content/themes/dotinstall/style.css
     </head>
     <body>
       <aside class="sidevar">

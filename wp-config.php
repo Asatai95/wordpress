683270f111eaa34ade97a5717254014dfb3ca635
Taishi Asato <defense433@gmail.com>
2019-11-02 19:26:14 +0900

    更新

--- wp-config.php
+++ wp-config.php
@@ -118,3 +118,7 @@ if ( !defined('ABSPATH') )
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
+
+
+define('WP_HOME','https://pockeawa.herokuapp.com');
+define('WP_SITEURL','https://pockeawa.herokuapp.com');

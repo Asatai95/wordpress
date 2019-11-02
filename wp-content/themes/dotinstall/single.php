bf70d30333cf449ca62e0ed82ba3a96b1cd8791d
Taishi Asato <defense433@gmail.com>
2019-11-03 00:04:26 +0900

    更新

--- wp-content/themes/dotinstall/single.php
+++ wp-content/themes/dotinstall/single.php
@@ -37,7 +37,7 @@
                 ?>
 
             </div><!-- /posts -->
-            <div class="main_information">
+            <div class="main_information single">
                 <div class="share_box">
                     <h2>Share</h2>
                     <p>僕らのラジオをシェア、いいね！してもらえるとうれしいです！</p>

3c903fdeda410d2a8e8afd74cd369983ebfdf3ab
Taishi Asato <defense433@gmail.com>
2019-11-22 13:06:26 +0900

    更新

--- wp-content/themes/dotinstall/index.php
+++ wp-content/themes/dotinstall/index.php
@@ -5,7 +5,7 @@
         <div class="top_main_img">
             <div class="back_movie">
                 <div class="main_top_view_video">
-                    <video src="https://res.cloudinary.com/hchyaihwv/video/upload/c_scale,h_500,w_375/v1574258843/my_media_bg.mp4" webkit-playsinline playsinline autoplay loop>
+                    <video src="https://res.cloudinary.com/hchyaihwv/video/upload/c_scale,h_820,w_420/v1574258843/my_media_bg.mp4" webkit-playsinline playsinline autoplay loop>
                     </video>
                     <div class="back_text">
                         <h2>Awamori in The Pocket</h2>
